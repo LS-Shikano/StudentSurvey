52568e226d8b5e1f550b1fa437ebfe9f37108d40
tetesoph <theresa.wagner@uni-konstanz.de>
2024-11-26 19:33:04 +0100

    add vignette experiment - first version

--- vignett_app/pages.py
+++ vignett_app/pages.py
@@ -1,18 +1,21 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
-from .models import Constants
+from .models import Constants, Player
 
 
-class MyPage(Page):
-    pass
+class HarassmentVignette(Page):
+    def vars_for_template(self):
+        return {'lang': self.participant.vars.get('language')}
+    form_model = Player
+    form_fields = ['harrassment']
 
 
-class ResultsWaitPage(WaitPage):
+class Manipulation(Page):
     pass
 
 
 class Results(Page):
     pass
+ 
 
-
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [Manipulation, HarassmentVignette]
